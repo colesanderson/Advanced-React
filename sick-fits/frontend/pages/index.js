af401ecd41a6b1cdd22176be5929fbf1f2b3dcae
Cole Sanderson <colesanderson4@gmail.com>
2018-11-05 20:26:50 -0800

    work up to lesson 12

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 const Home = props => (
     <div>
-      <p>Yo!</p>
-      <Link href="/sell">
-        <a>Go to Sell</a>
-      </Link>
+        <p>Yo!</p>
+        <Link href="/sell">
+            <a>Go to Sell</a>
+        </Link>
     </div>
-  );
+);
 
 export default Home;
